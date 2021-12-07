503c8a95fee3a699a2e74540786bcd4897d93a68
awt-256 <68395886+awt-256@users.noreply.github.com>
2021-12-07 17:12:21 -0500

    feat: WIP parseClass

--- index.js
+++ index.js
@@ -54,17 +54,18 @@ class WalkingView extends DataView {
     }
 }
 
-module.exports.rawParser = function rawParse(bufferLike) {
+function readClass(bufferLike) {
     const u8 = new Uint8Array(bufferLike);
     const buffer = u8.buffer;
     const view = new WalkingView(buffer);
 
-    const magic = view.getUint32(); // 0xCA FE BA BE
+    const magic = view.getUint32();
+    if (magic !== 0xcafebabe) throw new TypeError("Invalid magic. Probably not reading a .class file.")
     const minorVersion = view.getUint16();
     const majorVersion = view.getUint16();
 
-    const constantPool = Array(view.getUint16() - 1);
-    for (let i = 0; i < constantPool.length; ++i) {
+    const constantPool = Array(view.getUint16());
+    for (let i = 1; i < constantPool.length; ++i) {
         const tag = view.getUint8();
         const constant = constantPool[i] = { tag }
 
@@ -117,11 +118,11 @@ module.exports.rawParser = function rawParse(bufferLike) {
                 constant.descriptorIndex = view.getUint16();
                 break;
             case CONSTANT_Dynamic:
-                constant.bootstrap_method_attrIndex = view.getUint16();
+                constant.bootstrapMethodAttrIndex = view.getUint16();
                 constant.nameAndTypeIndex = view.getUint16();
                 break;
             case CONSTANT_InvokeDynamic:
-                constant.bootstrapMethod_attrIndex = view.getUint16();
+                constant.bootstrapMethodAttrIndex = view.getUint16();
                 constant.nameAndTypeIndex = view.getUint16();
                 break;
             case CONSTANT_Module:
@@ -201,3 +202,141 @@ module.exports.rawParser = function rawParse(bufferLike) {
     }
 }
 
+const ACC_PUBLIC            	  = 0x0001;
+const ACC_FINAL                   = 0x0010;
+const ACC_SUPER                   = 0x0020;
+const ACC_INTERFACE         	  = 0x0200;
+const ACC_ABSTRACT          	  = 0x0400;
+const ACC_SYNTHETIC         	  = 0x1000;
+const ACC_ANNOTATION              = 0x2000;
+const ACC_ENUM                    = 0x4000;
+const ACC_MODULE                  = 0x8000;
+
+const MAJOR_2_VERSION = {
+    45: "1.1",
+    46: "1.2",
+    47: "1.3",
+    48: "1.4",
+    49: "5.0",
+    50: "6",
+    51: "7",
+    52: "8",
+    53: "9",
+    54: "10",
+    55: "11",
+    56: "12",
+    57: "13",
+    58: "14",
+    59: "15",
+    60: "16"
+}
+
+function parseClass(classData) {
+    if (classData instanceof ArrayBuffer || classData instanceof Uint8Array) classData = readClass(classData);
+    const decoder = new TextDecoder();
+    const convo = ((convo) => {
+        const u8 = new Uint8Array(convo);
+        const i32 = new Int32Array(convo);
+        const i64 = new BigInt64Array(convo);
+        const f32 = new Float32Array(convo);
+        const f64 = new Float64Array(convo);
+        
+        const load = (buffer) => u8.set(buffer);
+
+        return {
+            toI32(buffer) {
+                load(buffer);
+                return i32[0]
+            },
+            toF32(buffer) {
+                load(buffer);
+                return f32[0]
+            },
+            toI64(buffer) {
+                load(buffer);
+                return i64[0]
+            },
+            toF64(buffer) {
+                load(buffer);
+                return f64[0]
+            }
+        }
+    })(new ArrayBuffer(8));
+
+    const version = MAJOR_2_VERSION[classData.majorVersion] || "unknown";
+
+    const accessFlags = {
+        PUBLIC: !!(classData.accessFlags & ACC_PUBLIC),
+        FINAL: !!(classData.accessFlags & ACC_FINAL),
+        SUPER: !!(classData.accessFlags & ACC_SUPER),
+        INTERFACE: !!(classData.accessFlags & ACC_INTERFACE),
+        ABSTRACT: !!(classData.accessFlags & ACC_ABSTRACT),
+        SYNTHETIC: !!(classData.accessFlags & ACC_SYNTHETIC),
+        ANNOTATION: !!(classData.accessFlags & ACC_ANNOTATION),
+        ENUM: !!(classData.accessFlags & ACC_ENUM),
+        MODULE: !!(classData.accessFlags & ACC_MODULE),
+    }
+
+    const constantPool = Array(classData.constantPool.length);
+    for (let i = 1; i < classData.constantPool.length; ++i) {
+        const constant = constantPool[i] = Object.assign({}, classData.constantPool[i]);
+
+        switch (constant.tag) {
+            case CONSTANT_Utf8:
+                constant.text = decoder.decode(constant.bytes)
+                break;
+            case CONSTANT_Integer:
+                constant.value = convo.toI32(constant.bytes);
+                break;
+            case CONSTANT_Float:
+                constant.value = convo.toF32(constant.bytes);
+                break;
+            case CONSTANT_Long:
+                constant.value = convo.toI64(constant.bytes);
+                i += 1;
+                break
+            case CONSTANT_Double:
+                constant.value = convo.toF64(constant.bytes);
+                i += 1;
+                break;
+            case CONSTANT_Class:
+                break;
+            case CONSTANT_String:
+                break;
+            case CONSTANT_Fieldref:
+                break;
+            case CONSTANT_Methodref:
+                break;
+            case CONSTANT_InterfaceMethodref:
+                break;
+            case CONSTANT_NameAndType:
+                break;
+            case CONSTANT_MethodHandle:
+                break;
+            case CONSTANT_MethodType:
+                break;
+            case CONSTANT_Dynamic:
+                break;
+            case CONSTANT_InvokeDynamic:
+                break;
+            case CONSTANT_Module:
+                break;
+            case CONSTANT_Package:
+                break;
+            default:
+                throw new SyntaxError("Invalid constant.")
+        }
+    }
+
+    const superClassName = classData.superClass === 0 ? "java/lang/Object" : decoder.decode(classData.constantPool[classData.constantPool[classData.superClass].nameIndex].bytes)
+
+    return {
+        raw: classData,
+        version,
+        accessFlags,
+        superClassName
+    }
+}
+
+module.exports.readClass = readClass;
+module.exports.parseClass = parseClass;
